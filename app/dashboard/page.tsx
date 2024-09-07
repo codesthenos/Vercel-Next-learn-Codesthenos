fc06a560078686d2fafc886b372afb4d6479b873
codesthenos <codesthenos@gmail.com>
2024-09-07 20:00:13 +0200

    descomentamos el componente revenue-chart.tsx, lo importamos, en dashboard, importamos la funcion fexhtRevenue y cambiamos el placeholder que habia puesto por el grafico

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue } from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 
-export default function Page() {
+export default async function Page() {
+  const revenue = await fetchRevenue()
+
   return (
     <main>
       <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>
@@ -18,6 +22,14 @@ export default function Page() {
         </div>
         <div>
           Total customers
+        </div>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <div>
+          <RevenueChart revenue={revenue} />
+        </div>
+        <div>
+          Latest invoices
         </div>
       </div>
     </main>
