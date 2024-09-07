93cc4d36b8267b7b69eb3f9e8af6164e2292247b
codesthenos <codesthenos@gmail.com>
2024-09-07 19:51:09 +0200

    Empezamos a editar /dashboard, le damos estilos responsive, placeholders por ahora

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,25 @@
+import { lusitana } from "@/app/ui/fonts";
+
 export default function Page() {
   return (
-    <p>Dashboard Page</p>
+    <main>
+      <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div>
+          Paid invoices
+        </div>
+        <div>
+          Pending invoices
+        </div>
+        <div>
+          Total invoices
+        </div>
+        <div>
+          Total customers
+        </div>
+      </div>
+    </main>
   );
 }
