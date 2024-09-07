c8dd08db1f787f9d42c226df1ed23fd0c49cde67
codesthenos <codesthenos@gmail.com>
2024-09-07 20:58:14 +0200

    Devuelvo en la base de datos el valor que habia en los placeholders al rpincipio pero usando la UI de vercel y una query de sql, cambio unos estilos que venian por defecto que yo creo que nno estan bien, ponian el grid template cols a 4 y 8 cuando solo queremos mostar 2 elementos maximo, el recent revenue y las last invoices, ademas tambien incluiomas las Card,a puntito de acabar el capitulo 7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,36 @@
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue } from "@/app/lib/data";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { Card } from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
   const revenue = await fetchRevenue()
+  const latestInvoices = await fetchLatestInvoices()
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices
+  } = await fetchCardData()
 
   return (
     <main>
       <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>
-        DashboardSSS
+        Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <div>
-          Paid invoices
-        </div>
-        <div>
-          Pending invoices
-        </div>
-        <div>
-          Total invoices
-        </div>
-        <div>
-          Total customers
-        </div>
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <div className="mt-6 grid grid-cols-1 gap-6 lg:grid-cols-2">
         <div>
           <RevenueChart revenue={revenue} />
         </div>
         <div>
-          Latest invoices
+          <LatestInvoices latestInvoices={latestInvoices} />
         </div>
       </div>
     </main>
