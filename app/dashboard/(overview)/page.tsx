2f7eebe53b0952f7ed7cfbb0e1c6f6f76ff35e09
codesthenos <codesthenos@gmail.com>
2024-09-07 23:44:56 +0200

    Incluimos suspense en last invoice, y en vez de usar las Cards, usamos el CardWrapper y lo metemos con suspense tambien ahora carga primero las invoice, luego las cards y luego la revenue, aunque si quito el timeout el revenue es lo primero, pero la cosa es que cada uno se renderiza enc uanto termina el fetch de forma individual

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,49 +1,32 @@
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  // Waterfall fetching
-  //const revenue = await fetchRevenue()
-  //const latestInvoices = await fetchLatestInvoices()
-  //const {
-  //  numberOfCustomers,
-  //  numberOfInvoices,
-  //  totalPaidInvoices,
-  //  totalPendingInvoices
-  //} = await fetchCardData()
-  
-  // Pararell fetching
-  const data = await Promise.all([fetchLatestInvoices(), fetchCardData()])
-
-  const latestInvoices = data[0]
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers
-  } = data[1]
-
   return (
     <main>
       <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 lg:grid-cols-2">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <div>
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
+        </div>
+        <div>
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+          </Suspense>
+        </div>
       </div>
     </main>
   );
