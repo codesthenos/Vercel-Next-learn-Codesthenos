9d0a686915f11a60294aa75edf72f9306df37893
codesthenos <codesthenos@gmail.com>
2024-09-07 10:07:01 +0200

    Empezando chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body>{children}</body>
