109af97c67eceb632d82921716853d78622c61cb
codesthenos <codesthenos@gmail.com>
2024-09-09 06:20:18 +0200

    Instalamos use-debounce e importamos useDebouncedCallback y envolvemos la funcion handleSearch en el callback de la funcion useDebouncedCallback, y como segundo parametro le pasamos el tiempo, por ahora medio segundo sin escirbir par aque haga el fetch

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,16 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams)
+    console.log(term)
 
     if (term) {
       params.set('query', term)
@@ -20,7 +22,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const searchQuery = params.get('query') ? `?search=${params.get('query')}` : ''
     
     replace(`${pathname}${searchQuery}`)
-  }
+  }, 500)
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
