fab91011897ec3549721cd9a9b0ae351b40b950d
codesthenos <codesthenos@gmail.com>
2024-09-09 05:36:39 +0200

    Empiezo con la pagina invoices, introducimos metodo de busqueda, por ahora solo se actualiza la url con el input, pero no busca nada, juego un poco con los params, y en vez de pintar la url que marcan en el video, hago una ligera modificacion, si existen parametros de busqueda, la url es invoice?search=params en vez de invoices?query=params, y ademas, si no hay parametros, se muestra invoices en vez de invoices?search=null

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,18 @@
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Search from "@/app/ui/search";
 
 export default async function Page() {
   return (
-    <RevenueChart />
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2x1`}>Invoices</h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+    </div>
   );
 }
