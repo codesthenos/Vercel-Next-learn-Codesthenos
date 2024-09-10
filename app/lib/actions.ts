7af4eb8250b5c2c4190340c186e9a11ca9b0c418
codesthenos <codesthenos@gmail.com>
2024-09-10 18:16:08 +0200

    Dejo tanto el raw data como el validated para que se vean las diferencias en el console.log

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,20 @@
 'use server'
 
+import { z } from 'zod'
+import { customers } from './placeholder-data'
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string()
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
 export async function createInvoice(formData:FormData) {
+  // raw data not validated
   const rawFormData = {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -10,4 +24,17 @@ export async function createInvoice(formData:FormData) {
   console.log(typeof rawFormData.customerId)
   console.log(typeof rawFormData.amount)
   console.log(typeof rawFormData.status)
+
+  // validated data with zod
+  const validatedData = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+  const {customerId, amount, status } = validatedData
+
+  console.log(validatedData)
+  console.log(typeof customerId)
+  console.log(typeof amount)
+  console.log(typeof status)
 }
