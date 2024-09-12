99daec9593fff89035ead6b5a841b7e8c7db4792
codesthenos <codesthenos@gmail.com>
2024-09-12 16:01:45 +0200

    Lanzo un error al principio de la funcion delete invoice para ver como se ve y funciona la pagina creada error.tsx dentro de la ruta dashboard/invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -47,10 +47,14 @@ export async function createInvoice(formData:FormData) {
 
   // insertamos en la base de datos el objeto compuesto de los datos obtenidos
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
+  } catch (error) {
+    return { message: 'Database Error: Failed to Create Invoice.' }
+  }
 
   // esto es para limpiar las cookies al volver a invoices y asegurarnos hacer el fetch para traer la creada
   revalidatePath('/dashboard/invoices')
@@ -70,18 +74,29 @@ export async function updateInvoice(id: string, formData: FormData) {
   })
   const amountInCents = amount * 100
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.'}
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice (id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`
+  throw new Error('Failed to Delete Invoice')
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
 
-  revalidatePath('/dashboard/invoices')
+    revalidatePath('/dashboard/invoices')
+
+    return { message: 'Deleted Invoice.' }
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
 }
